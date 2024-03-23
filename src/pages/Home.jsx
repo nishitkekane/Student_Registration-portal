430c4e53aa7ca81c01e121f3d6e453e46630f650
nishitkekane <nishit.kekane04@gmail.com>
2024-03-23 17:39:17 +0530

    Bug Fixes!

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,14 @@ import Student from '../components/addStudent'
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 const Home = () => {
-  const role = 'ADMIN'
+  const role = localStorage.getItem('role')
   const token = localStorage.getItem('token')
   const navigate = useNavigate()
 
-  // useEffect(() => {
-  //   if (!token) navigate('/login')
-  // }, [token])
-  return <>{ <Teacher />}</>
+  useEffect(() => {
+    if (!token) navigate('/login')
+  }, [token])
+  return <>{token&& role==="ADMIN"? <Teacher />:<Student/>}</>
 }
 
 export default Home
